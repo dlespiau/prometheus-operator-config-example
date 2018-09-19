cda53981506eba30cce65630feff90e1ab68783f
Damien Lespiau <damien@weave.works>
2018-09-19 12:04:36 +0100

    kube-prometheus: Add default images and versions

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# This script uses arg $1 (name of *.jsonnet file to use) to generate the manifests/*.yaml files.
+
+set -e
+set -x
+# only exit with zero if all commands of the pipeline exit successfully
+set -o pipefail
+
+# Make sure to start with a clean 'manifests' dir
+rm -rf manifests
+mkdir manifests
+
+jsonnet -J vendor -m manifests "${1-kube-prometheus.jsonnet}" | xargs -I{} sh -c 'cat {} | gojsontoyaml > {}.yaml; rm -f {}' -- {}
